fa4d9299bc03ff493256a023cef9b2ab10e179fa
Vladik Leshko <leshkovladik@gmail.com>
2022-12-20 19:55:54 +0300

    fixes

--- js/number_input.js
+++ js/number_input.js
@@ -1,9 +1,27 @@
 
 var input = document.getElementById("number_input");
-
-input.onfocus = function() {
+var input1 = document.getElementById("number_input1");
+input.onmouseenter = function() {
     "use strict";
     if(this.placeholder == "Ваш телефон") {
-        this.value = '+7 (___) _ -  - __';
+        this.placeholder = '+7 (___) _ -  - __';
+    }
+};
+input.onmouseleave = function() {
+    "use strict";
+    if(this.placeholder == '+7 (___) _ -  - __') {
+        this.placeholder = 'Ваш телефон';
+    }
+};
+input1.onmouseenter = function() {
+    "use strict";
+    if(this.placeholder == "Ваш телефон") {
+        this.placeholder = '+7 (___) _ -  - __';
+    }
+};
+input1.onmouseleave = function() {
+    "use strict";
+    if(this.placeholder == '+7 (___) _ -  - __') {
+        this.placeholder = 'Ваш телефон';
     }
 };
